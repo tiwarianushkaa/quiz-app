39c2a4b19ba317bd260367d8050c035b974936fb
tiwarianushkaa <tiwarianushka0803@gmail.com>
2024-01-19 18:25:58 +0530

    Merge branch 'main' of https://github.com/tiwarianushkaa/quiz-app

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+<<<<<<< HEAD
+import 'package:flutter/material.dart';
+
+import 'package:adv_basics/quiz.dart';
+
+void main() {
+  runApp(const Quiz());
+=======
+import 'package:flutter/material.dart';
+
+import 'package:adv_basics/quiz.dart';
+
+void main() {
+  runApp(const Quiz());
+>>>>>>> 3c329d9f985065b8d107608893b4fb482578bfc9
+}
---
+++ lib/models/quiz_question.dart
@@ -0,0 +1,26 @@
+<<<<<<< HEAD
+class QuizQuestion {
+  const QuizQuestion(this.text, this.answers);
+
+  final String text;
+  final List<String> answers;
+
+  List<String> getShuffledAnswers() {
+    final shuffledList = List.of(answers);
+    shuffledList.shuffle();
+    return shuffledList;
+  }
+=======
+class QuizQuestion {
+  const QuizQuestion(this.text, this.answers);
+
+  final String text;
+  final List<String> answers;
+
+  List<String> getShuffledAnswers() {
+    final shuffledList = List.of(answers);
+    shuffledList.shuffle();
+    return shuffledList;
+  }
+>>>>>>> 3c329d9f985065b8d107608893b4fb482578bfc9
+}
